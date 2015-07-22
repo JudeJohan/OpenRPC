828f44e186c946ee1d1b1a9d065e0a375cb25b10
Tubbles <jae91m@gmail.com>
2015-07-22 21:30:50 +0200

    soon to clean up the project, move to Qt Creator and its specific project files, instead of Eclipse

--- src/OpenRPC.cpp
+++ src/OpenRPC.cpp
@@ -6,11 +6,20 @@
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
-#include <iostream>
+#include "orpcApp.h"
+#include <wx/event.h>
 
 using namespace std;
 
 int main() {
+	wxBEGIN_EVENT_TABLE(MyFrame, wxFrame)
+	    EVT_MENU(ID_Hello,   orpcFrame::OnHello)
+	    EVT_MENU(wxID_EXIT,  orpcFrame::OnExit)
+	    EVT_MENU(wxID_ABOUT, orpcFrame::OnAbout)
+	wxEND_EVENT_TABLE()
+
+
+
 	cout << "!!!Hello World!!!" << endl;
 	return 0;
 }
